1b5d293e3c8fc67b134ba75f2943e0ef1637c5a2
Piotr Brzezina <piotr.brzezina@thesoftwarehouse.pl>
2017-10-09 01:06:42 +0200

    add serialization group

--- src/AppBundle/Entity/Project.php
+++ src/AppBundle/Entity/Project.php
@@ -4,13 +4,16 @@ namespace AppBundle\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
  *     attributes={
  *          "filters"={
  *              "project.order_filter"
- *          }
+ *          },
+ *          "normalization_context" = { "groups" = { "project_read" }},
+ *          "denormalization_context" = { "groups" = { "project_write" }}
  *     }
  * )
  * @ORM\Table()
@@ -24,45 +27,58 @@ class Project
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @Groups({"project_read"})
      */
     private $id;
 
     /**
      * @var string
      * @ORM\Column(type="string", length=100)
+     *
+     * @Groups({"project_write", "project_read"})
      */
     private $number;
 
     /**
      * @var string
      * @ORM\Column(type="string", length=100)
+     *
+     * @Groups({"project_write", "project_read"})
      */
     private $name;
 
     /**
      * @var \DateTime
      * @ORM\Column(type="decimal", precision=10, scale=2)
+     *
+     * @Groups({"project_write", "project_read"})
      */
     private $budget;
 
     /**
+     * @var User[]
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User", mappedBy="project")
+     *
+     * @Groups({"project_write", "project_read"})
+     */
+    private $users;
+
+    /**
      * @var string
      * @ORM\Column(type="string", length=100)
+     *
+     * @Groups({"project_read"})
      */
     private $status;
 
-    /**
-     * @var User[]
-     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\User", mappedBy="project")
-     */
-    private $users;
-    
     /**
      * Constructor
      */
     public function __construct()
     {
         $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->setStatus('new');
     }
 
     /**
